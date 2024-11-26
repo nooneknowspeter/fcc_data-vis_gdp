fd1c4a70ad214179035d5cae3fac8fbb63aec08d
peter <jerelepeter06@gmail.com>
2024-11-26 15:30:06 -0400

    added title and source

--- src/app/components/visualization/BarChart.tsx
+++ src/app/components/visualization/BarChart.tsx
@@ -5,7 +5,7 @@ import { useRef, useEffect } from "react";
 //npm i --save-dev @types/d3
 
 const BarChart = () => {
-  const data = [5000, 50000, 500, 1, 1, 1, 1];
+  const data = [5000, 50000, 500, 6000, 4050, 3050, 200];
   const width = 640;
   const height = 400;
   const marginTop = 20;
@@ -15,30 +15,50 @@ const BarChart = () => {
 
   const gx = useRef<SVGGElement>(null);
   const gy = useRef<SVGGElement>(null);
+
   // x scale
   const x = d3.scaleLinear(
     [0, data.length - 1],
     [marginLeft, width - marginRight],
   );
+
   // y scale
-  const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
+  const y = d3.scaleLinear(
+    [0, d3.max(data)],
+    [height - marginBottom, marginTop],
+  );
 
-  const line = d3.line((d: number, i: number) => x(i), y);
+  const line = d3.line((d, i) => x(i), y);
 
-  useEffect(() => void d3.select(gx.current).call(d3.axisBottom(x)), [gx, x]);
-  useEffect(() => void d3.select(gy.current).call(d3.axisLeft(y)), [gy, y]);
+  useEffect(() => {
+    if (gx.current) d3.select(gx.current).call(d3.axisBottom(x));
+  }, [gx, x]);
+
+  useEffect(() => {
+    if (gx.current) d3.select(gy.current).call(d3.axisLeft(y));
+  }, [gy, y]);
 
   return (
     <>
-      <div className="flex h-screen content-center items-center justify-center">
+      <div className="flex h-screen flex-col content-center items-center justify-center">
+        <h1
+          id="title"
+          className="font-bold text-neutral-900 dark:text-neutral-200"
+        >
+          Chart Showing United States GDP 1947 - 2015
+        </h1>
         <svg width={width} height={height}>
-          <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
-          <g ref={gy} transform={`translate(${marginLeft},0)`} />
+          <g
+            ref={gx}
+            transform={`translate(0,${height - marginBottom})`}
+            id="x-axis"
+          />
+          <g ref={gy} transform={`translate(${marginLeft},0)`} id="y-axis" />
           <path
             fill="none"
             stroke="currentColor"
             strokeWidth="1.5"
-            d={line(data)}
+            d={line(data) || ""}
           />
           <g fill="white" stroke="currentColor" strokeWidth="1.5">
             {data.map((d, i) => (
@@ -46,6 +66,13 @@ const BarChart = () => {
             ))}
           </g>
         </svg>
+        <a
+          className="text-sm text-neutral-900 dark:text-neutral-200"
+          href="https://fred.stlouisfed.org/"
+          target="_blank"
+        >
+          Source: Federal Reserve Economic Data
+        </a>
       </div>
     </>
   );
