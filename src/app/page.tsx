fed4e96b99b67b4a7f7046f5e274c2d9262e9fb5
peter <jerelepeter06@gmail.com>
2024-11-26 12:57:55 -0400

    npm i tailwind prettier plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center">working?</h1>
+      <h1 className="bg-sky-700 px-4 py-2 text-center text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        working?
+      </h1>
     </>
   );
 }
