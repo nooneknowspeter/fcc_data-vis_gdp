25927fafd767af7ab02436c2693075738ed6444f
peter <jerelepeter06@gmail.com>
2024-11-26 12:59:53 -0400

    npm i d3js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="bg-sky-700 px-4 py-2 text-center text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        working?
-      </h1>
+      <h1 className="text-center">working?</h1>
     </>
   );
 }
