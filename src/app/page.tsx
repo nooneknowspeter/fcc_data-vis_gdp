56b69bd209176bb3a597ead450aebccefafafc2d
peter <jerelepeter06@gmail.com>
2024-11-26 13:58:56 -0400

    npm i --save-dev @types/d3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { BarChart } from "./components/Components";
 export default function Home() {
   return (
     <>
-      <h1 className="text-center">working?</h1>
       <BarChart />
     </>
   );
